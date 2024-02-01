a5b96081200bb9515595b676bf062780f8053021
Md Asaduzaman <asadasad9282@gmail.com>
2024-02-01 16:20:31 +0600

    swap section design

--- src/app/(dashboardLayout)/Swap/page.js
+++ src/app/(dashboardLayout)/Swap/page.js
@@ -1,23 +1,23 @@
 import Image from "next/image";
 import React from "react";
 import coin1 from "@/assets/coin1.png";
-import pmlogo from "@/assets/pmlogo.png";
+import mind from "@/assets/coin1.png";
+import pmind from "@/assets/pmlogo.png";
+import arrow from "@/assets/arrowDuble.png";
 
 const Swap = () => {
   return (
-    <div>
-      <div
-        className='w-[654px] h-[707px] bg-[#EDEDED
-]'
-      >
-        <div className=' relative  text-sm w-390px] h-10 '>
+    <div className='w-[654px] px-11 pt-11 pb-10 h-[707px] bg-[#EDEDED]'>
+      <div className='flex items-center gap-[18px] flex-col   mx-auto'>
+        {/* User Wallet Address */}
+        <div className=' relative   text-sm w-[390px] h-10 '>
           <Image
             className='absolute w-[27px]    top-1/2 left-5 transform -translate-x-1/2 -translate-y-1/2 h-[27px] flex place-items-center items-center'
             src={coin1}
             alt=''
           />
           <select
-            className='custom-select ps-10 flex place-items-center border-[1px] rounded-3xl  px-4 text-sm w-full h-10  bg-[#EDEDED] focus:outline-none'
+            className='custom-select ps-10 flex place-items-center border-[1px] rounded-3xl  px-4 text-sm w-full h-10    focus:outline-none'
             name=''
             id=''
           >
@@ -32,14 +32,16 @@ const Swap = () => {
             </option>
           </select>
         </div>
-        <div className='pt-[18px] relative  text-sm w-390px] h-10 '>
+
+        {/* Mind Chain Network */}
+        <div className=' relative   text-sm w-[390px] h-10 '>
           <Image
             className='absolute w-[27px]    top-1/2 left-5 transform -translate-x-1/2 -translate-y-1/2 h-[27px] flex place-items-center items-center'
             src={coin1}
             alt=''
           />
           <select
-            className='custom-select ps-10 flex place-items-center border-[1px] rounded-3xl  px-4 text-sm w-full h-10  bg-[#EDEDED] focus:outline-none'
+            className='custom-select ps-10 flex place-items-center border-[1px] rounded-3xl  px-4 text-sm w-full h-10    focus:outline-none'
             name=''
             id=''
           >
@@ -54,36 +56,90 @@ const Swap = () => {
             </option>
           </select>
         </div>
-        <div className='mt-9'>
-          <h2 className='font-semibold'>Swap Form</h2>
-          <div className='mt-6'>
-            <div className='lg:mx-8 flex place-items-center gap-1 mt-12 '>
-              <image className='' src={pmlogo} alt='' />
-              <select
-                className=' h-[55px] border-none outline-none  bg-[#EDEDED] text-primary'
-                name=''
-                id=''
-              >
-                <option className=' text-primary opacity-50' value='PMIND'>
-                  PMIND
-                </option>
-                <option className=' text-primary opacity-50' value='USD'>
-                  USD
-                </option>
-                <option className=' text-primary opacity-50' value='EUR'>
-                  EUR
-                </option>
-                <option className=' text-primary opacity-50' value='GBP'>
-                  GBP
-                </option>
-                <option className=' text-primary opacity-50' value='JPY'>
-                  JPY
-                </option>
-                <option className=' text-primary opacity-50' value='AUD'>
-                  AUD
-                </option>
-              </select>
+      </div>
+
+      {/* *************** */}
+      {/* Swap Form */}
+      {/* *************** */}
+      <div className='mt-9'>
+        <h2 className='font-semibold pb-5'>Swap Form</h2>
+        <div className=' flex place-items-center text-start border-[1px] bg-white border-[#FF971D] w-[563px] h-[55px]  rounded-2xl '>
+          <div className='relative   bg-[#EDEDED] flex place-items-center gap-1   '>
+            <div className=' absolute px-3'>
+              <Image className='' src={mind} alt='aa' />
             </div>
+            <select
+              className='custom-select  focus:outline-none h-[55px] ps-12 font-semibold  w-[150px] border-[1px] border-[#FF971D] rounded-s-2xl'
+              name=''
+              id=''
+            >
+              <option className=' text-primary opacity-50' value='MIND'>
+                MIND
+              </option>
+              <option className=' text-primary opacity-50' value='USD'>
+                USD
+              </option>
+              <option className=' text-primary opacity-50' value='EUR'>
+                EUR
+              </option>
+              <option className=' text-primary opacity-50' value='GBP'>
+                GBP
+              </option>
+              <option className=' text-primary opacity-50' value='JPY'>
+                JPY
+              </option>
+              <option className=' text-primary opacity-50' value='AUD'>
+                AUD
+              </option>
+            </select>
+          </div>
+          <div className=' flex justify-start ps-6 text-[#909090] bg-white'>
+            <p className=' font-semibold text-lg  '> 00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-9 flex justify-end'>
+        <Image src={arrow} alt='' />
+      </div>
+      {/* ****************/}
+      {/* Swap to */}
+      {/* ****************/}
+
+      <div>
+        <h2 className='font-semibold pb-5'>Swap to</h2>
+        <div className=' flex place-items-center text-start border-[1px] bg-white border-[#FF971D] w-[563px] h-[55px]  rounded-2xl '>
+          <div className='relative   bg-[#EDEDED] flex place-items-center gap-1   '>
+            <div className=' absolute p-3'>
+              <Image className='' src={pmind} alt='aa' />
+            </div>
+            <select
+              className='custom-select h-[55px] focus:outline-0 ps-12 font-semibold  w-[150px] border-[1px] border-[#FF971D] rounded-s-2xl'
+              name=''
+              id=''
+            >
+              <option className=' text-primary opacity-50' value='MIND'>
+                MIND{" "}
+              </option>
+              <option className=' text-primary opacity-50' value='USD'>
+                USD
+              </option>
+              <option className=' text-primary opacity-50' value='EUR'>
+                EUR
+              </option>
+              <option className=' text-primary opacity-50' value='GBP'>
+                GBP
+              </option>
+              <option className=' text-primary opacity-50' value='JPY'>
+                JPY
+              </option>
+              <option className=' text-primary opacity-50' value='AUD'>
+                AUD
+              </option>
+            </select>
+          </div>
+          <div className=' flex justify-start ps-6 text-[#909090] bg-white'>
+            <p className=' font-semibold text-lg  '> 00</p>
           </div>
         </div>
       </div>
