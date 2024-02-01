1586ab631151615b5e8c79cb33eecb3f4dbdc330
Md Asaduzaman <asadasad9282@gmail.com>
2024-02-01 16:37:17 +0600

    swap page desing

--- src/app/(dashboardLayout)/Swap/page.js
+++ src/app/(dashboardLayout)/Swap/page.js
@@ -143,6 +143,36 @@ const Swap = () => {
           </div>
         </div>
       </div>
+
+      <div className='mt-[18px]'>
+        <span className='font-medium text-lg'>
+          MIND verified on 17 sources.
+        </span>
+        <span className='font-light ps-2 text-[#2E2E2E] text-xs'>
+          Always confirm the token address on a{" "}
+        </span>
+        <p className='font-bold mt-3 text-[#FF971D] text-[]'>block explorer.</p>
+      </div>
+      <div className='mt-8'>
+        <div className='flex gap-3 justify-center'>
+          <p className='font-bold text-lg'>Advance Option</p>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='25'
+            height='25'
+            viewBox='0 0 25 25'
+            fill='none'
+          >
+            <path
+              d='M19.4896 8.55214C19.3928 8.45451 19.2775 8.37701 19.1506 8.32413C19.0237 8.27125 18.8875 8.24402 18.75 8.24402C18.6125 8.24402 18.4763 8.27125 18.3494 8.32413C18.2225 8.37701 18.1073 8.45451 18.0104 8.55214L13.2396 13.323C13.1428 13.4206 13.0275 13.4981 12.9006 13.551C12.7737 13.6039 12.6375 13.6311 12.5 13.6311C12.3625 13.6311 12.2263 13.6039 12.0994 13.551C11.9725 13.4981 11.8573 13.4206 11.7604 13.323L6.9896 8.55214C6.89276 8.45451 6.77755 8.37701 6.65061 8.32413C6.52368 8.27125 6.38752 8.24402 6.25001 8.24402C6.1125 8.24402 5.97635 8.27125 5.84941 8.32413C5.72247 8.37701 5.60726 8.45451 5.51043 8.55214C5.31642 8.74731 5.20752 9.01132 5.20752 9.28652C5.20752 9.56171 5.31642 9.82572 5.51043 10.0209L10.2917 14.8021C10.8776 15.3874 11.6719 15.7161 12.5 15.7161C13.3281 15.7161 14.1224 15.3874 14.7083 14.8021L19.4896 10.0209C19.6836 9.82572 19.7925 9.56171 19.7925 9.28652C19.7925 9.01132 19.6836 8.74731 19.4896 8.55214Z'
+              fill='#000'
+            />
+          </svg>
+        </div>
+        <p className=' flex justify-center mt-5 rounded-2xl place-items-center text-white text-lg font-bold  text-center  h-14 bg-[#FF971D]'>
+          Get Quotes
+        </p>
+      </div>
     </div>
   );
 };
